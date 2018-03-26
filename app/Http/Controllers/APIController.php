a986ede9a0950c942300d5b5a91b3e84ad557c82
Harris Christiansen (hc15ret2) <harrischristiansen@mac.com>
2018-03-25 23:36:41 -0400

    Start Bridge Discovery

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -7,6 +7,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 use App\Models\Bridge;
 use App\Models\Light;
@@ -21,6 +22,35 @@ class APIController extends Controller {
 	
 	public function getBridge(Bridge $bridge) {
 		return $bridge;
+	}
+	
+	// --------------- Bridge Discovery ---------------
+	
+	public function getLocalBridges() {
+		$client = new Client();
+		$r = $client->get('https://www.meethue.com/api/nupnp');
+		if ($r->getStatusCode() != 200) {
+			return [];
+		}
+		return json_decode($r->getBody());
+		
+	}
+	
+	public function addBridge($ipAddr) {
+		$client = new Client();
+		$r = $client->post("http://".$ipAddr."/api", [
+			'body' => '{"devicetype":"my_hue_app#iphone controlshow"}'
+		]);
+		if ($r->getStatusCode() != 200) {
+			return ['error' => 'request failed'];
+		}
+		$result = json_decode($r->getBody(), true);
+		foreach ($result as $item) {
+			if (isset($item['error'])) {
+				return ['error' => $item['error']];
+			}
+		}
+		return ['success' => 'added'];
 	}
 	
 	// --------------- Lights ---------------
--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// Bridge Discovery
+Route::get('bridges', 'APIController@getLocalBridges')->name('api_local_bridges');
+Route::get('addBridge/{ipAddr}', 'APIController@addBridge')->name('api_add_bridge');
+
+// Bridges
 Route::group(['prefix' => 'bridge'], function () {
 	Route::get('all', 'APIController@getBridges')->name('api_bridges');
 	Route::group(['prefix' => '{bridge}'], function () {
@@ -19,6 +24,7 @@ Route::group(['prefix' => 'bridge'], function () {
 	});
 });
 
+// Lights
 Route::group(['prefix' => 'light'], function () {
 	Route::get('all', 'APIController@getLights')->name('api_lights');
 	Route::group(['prefix' => '{light}'], function () {
