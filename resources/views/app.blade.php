faf5678c3d984d4fc675f6ee3c374cd43d7bf068
Harris Christiansen (hc15ret2) <harrischristiansen@mac.com>
2018-04-30 00:04:34 -0400

    Fix jQuery import

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -81,7 +81,7 @@
 	<!-- ========== Javascript ========== -->
 	
 	<!-- jQuery / jQuery UI -->
-	<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
 	<script type="text/javascript" src="/js/plugins/jquery.cookie.js"></script>
 	
